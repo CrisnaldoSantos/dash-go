cc278165fb9998778490623c158d99dd1128513c
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-01-30 16:41:45 -0300

    feat: create login page and fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,61 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
-    <h1>dash go</h1>
-  )
-}
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              size="lg"
+              _hover={{
+                bgColor: 'gray.900',
+              }}
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Home
+export default Home;
